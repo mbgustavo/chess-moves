f8c71d0a22eed7032546d79a502eb16a2bcb3fa2
mbgustavo <mbtt.gustavo@gmail.com>
2021-01-09 03:53:43 -0300

    Enable other pieces

--- src/collections/pieces.ts
+++ src/collections/pieces.ts
@@ -5,22 +5,22 @@ const pieces: { [key: string]: Piece } = {
   horse: {
     possibleMoves: [{ x: 2, y: 1 }, { x: 1, y: 2 }],
     multipleMoves: false
-  // },
-  // knight: {
-  //   possibleMoves: [{ x: 1, y: 0 }, { x: 0, y: 1 }],
-  //   multipleMoves: true
-  // },
-  // bishop: {
-  //   possibleMoves: [{ x: 1, y: 1 }],
-  //   multipleMoves: true
-  // },
-  // queen: {
-  //   possibleMoves: [{ x: 1, y: 1 }, { x: 1, y: 0 }, { x: 0, y: 1 }],
-  //   multipleMoves: true
-  // },
-  // king: {
-  //   possibleMoves: [{ x: 1, y: 1 }, { x: 1, y: 0 }, { x: 0, y: 1 }],
-  //   multipleMoves: false
+  },
+  knight: {
+    possibleMoves: [{ x: 1, y: 0 }, { x: 0, y: 1 }],
+    multipleMoves: true
+  },
+  bishop: {
+    possibleMoves: [{ x: 1, y: 1 }],
+    multipleMoves: true
+  },
+  queen: {
+    possibleMoves: [{ x: 1, y: 1 }, { x: 1, y: 0 }, { x: 0, y: 1 }],
+    multipleMoves: true
+  },
+  king: {
+    possibleMoves: [{ x: 1, y: 1 }, { x: 1, y: 0 }, { x: 0, y: 1 }],
+    multipleMoves: false
   }
 }
 
